dd2de39eb75c46f668e83c2d68318714014b4f5a
Max Favilli <max.favilli@gmail.com>
2012-08-26 18:34:23 +0200

    Fixed bug.

--- bootstrap-tagmanager.js
+++ bootstrap-tagmanager.js
@@ -209,6 +209,7 @@
             );
          obj.before(html);
          jQuery("#myRemover_" + TagId).on("click", obj, function (e) {
+			e.preventDefault();
             var TagIdToRemove = parseInt(jQuery(this).attr("TagIdToRemove"));
 
             e.data.spliceTag(TagIdToRemove,e.data);
