512e95f8c9f9979e7a3de89102d55ca293069f76
Bijk <actionmanager@gmail.com>
2014-05-01 12:26:38 -0700

    Update
    Fix for option externalTagId: true 
    Can't remove tag with custom ID  # var TagIdToRemove = parseInt($(this).attr("TagIdToRemove"));
    
    + tagsManager('tags_id') Return ID of tag elements

--- tagmanager.js
+++ tagmanager.js
@@ -155,7 +155,7 @@
 
                 $el.find("#" + newTagRemoveId).on("click", $self, function(e) {
                     e.preventDefault();
-                    var TagIdToRemove = parseInt($(this).attr("TagIdToRemove"));
+                    var TagIdToRemove = $(this).attr("TagIdToRemove");
                     privateMethods.spliceTag.call($self, TagIdToRemove, e.data);
                 });
 
@@ -212,6 +212,12 @@
 
         tags : function() {
             var $self = this, tlis = $self.data("tlis");
+            return tlis;
+        },
+        
+        // Return tags ID
+		tags_id: function() { 		
+            var $self = this, tlis = $self.data("tlid");
             return tlis;
         }
     },
